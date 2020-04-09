229a286d1db7b4406be4d82975db4921d13cc188
goose756 <goose756@yahoo.com>
2020-04-08 22:28:18 -0700

    Checkin fix.

--- SpotifyFunTime.Application/SpotifyService.cs
+++ SpotifyFunTime.Application/SpotifyService.cs
@@ -14,16 +14,25 @@ namespace SpotifyFunTime.Application
             _client = client;
         }
 
-        public async Task<ApiResponse<User>> GetCurrentUser(TokenSet tokenSet)
-        {
-            var userResponse = await _client.SendAsync<User>(tokenSet, HttpMethod.Get, "me");
+        public async Task<ApiResponse<User>> GetCurrentUser(TokenSet tokenSet) =>
+            await _client.SendAsync<User>(tokenSet, HttpMethod.Get, "me");
 
-            return userResponse;
-        }
+        //TODO: Results of this aren't accurate - see GitHub issue https://github.com/spotify/web-api/issues/1441
+        public async Task<ApiResponse<Paging<PlayHistory>>> GetLastTenPlayedTracks(TokenSet tokenSet) =>
+            await _client.SendAsync<Paging<PlayHistory>>(tokenSet, HttpMethod.Get, "me/player/recently-played");
+
+        public async Task<ApiResponse<Paging<Track>>> GetTopTracks(TokenSet tokenSet, string timeRange, int limit) =>
+            await _client.SendAsync<Paging<Track>>(tokenSet, HttpMethod.Get, $"me/top/tracks?limit={limit}&time_range={timeRange}");
+
+        public async Task<ApiResponse<Paging<Artist>>> GetTopArtists(TokenSet tokenSet, string timeRange, int limit) =>
+            await _client.SendAsync<Paging<Artist>>(tokenSet, HttpMethod.Get, $"me/top/artists?limit={limit}&time_range={timeRange}");
     }
 
     public interface ISpotifyService
     {
         Task<ApiResponse<User>> GetCurrentUser(TokenSet tokenSet);
+        Task<ApiResponse<Paging<PlayHistory>>> GetLastTenPlayedTracks(TokenSet tokenSet);
+        Task<ApiResponse<Paging<Track>>> GetTopTracks(TokenSet tokenSet, string timeRange, int limit);
+        Task<ApiResponse<Paging<Artist>>> GetTopArtists(TokenSet tokenSet, string timeRange, int limit);
     }
 }
